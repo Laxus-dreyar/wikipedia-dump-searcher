2c19a3a9226cdb84ae1f60707ce4a4576fd6ed8c
Mrinal <mrinalkhubchandani13@gmail.com>
2021-08-27 11:34:40 +0530

    changing to ET

--- wiki_search_eng/assignment.py
+++ wiki_search_eng/assignment.py
@@ -1,21 +1,33 @@
 import xml.sax
+import time
+import xml.etree.ElementTree as ET
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
 class IndexHandler( xml.sax.ContentHandler ):
 
     def __init__(self):
-
         self.curID = 1
-        self.curTag = ""
-        self.docID = ""
-        self.title = ""
-        self.infoBox = ""
-        self.body = ""
-        self.category = ""
-        self.links = ""
-        self.references = ""
-    
+        self.curTag = ''
+        self.docID = ''
+        self.title = ''
+        self.infoBox = ''
+        self.body = ''
+        self.category = ''
+        self.links = ''
+        self.references = ''
+        self.text = ''
+        self.un_list = []
+        self.time = 0
+
+    def startDocument(self):
+        self.time = time.time()
+
     def startElement(self,name,attributes):
         self.curTag = name
+        if name not in self.un_list:
+            self.un_list.append(name)
+        
         if name == 'page':
             self.docID = self.curID
             self.curID = self.curID + 1
@@ -24,33 +36,40 @@ class IndexHandler( xml.sax.ContentHandler ):
         if name == 'page':
             self.curTag = ''
             self.title = ''
-            self.infoBox = ""
-            self.body = ""
-            self.category = ""
-            self.links = ""
-            self.references = ""
+            self.infoBox = ''
+            self.body = ''
+            self.category = ''
+            self.links = ''
+            self.references = ''
+            
+        
+        if name == 'text':
+            self.text = ''
         
         if name == 'title':
-            print(self.title)
+            self.title = ''
+            # print(self.curID)
+        
+        # if name == 'body':
+        #     x = self.remove_stop(self.body)
+        
+        # if name == 'title':
+        #     print(self.title)
     
     def characters(self, content):
         if self.curTag == 'title':
-            self.title = content
-
-        if self.curTag == 'iBox':
-            self.infoBox = content.lower()
-        
-        if self.curTag == 'body':
-            self.body == content.lower()
-        
-        if self.curTag == 'cate':
-            self.category = content.lower()
-        
-        if self.curTag == 'links':
-            self.links = content
-        
-        if self.curTag == 'ref':
-            self.references =content
+            self.title += content
+        else:
+            self.text += content
+    
+    def endDocument(self):
+        print(time.time()-self.time)
+    
+    def remove_stop(self,str):
+        stop_words = list(stopwords.words('english'))
+        stop_words.append(".")
+        out = [w for w in word_list if not w in stop_words]
+        return out
     
 
 
@@ -58,4 +77,15 @@ parser = xml.sax.make_parser()
 parser.setFeature(xml.sax.handler.feature_namespaces, 0)
 indexer = IndexHandler()
 parser.setContentHandler(indexer)
-parser.parse('../wiki-dump.xml')
+parser.parse('../dump.xml')
+
+
+# tree = ET.parse('../wiki-dump.xml')
+# root = tree.getroot()
+# un = []
+# for elem in root.iter():
+#     if elem.tag not in un:
+#         un.append(elem.tag)
+
+# for i in un:
+#     print(i)
