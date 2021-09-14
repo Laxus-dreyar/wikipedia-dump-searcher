d1da4b16a8be9cb4220099fee8d4c117985b1b9d
Mrinal <mrinalkhubchandani13@gmail.com>
2021-09-15 04:54:22 +0530

    ranking done

--- wiki_search_eng/search.py
+++ wiki_search_eng/search.py
@@ -9,7 +9,10 @@ import heapq
 snow_stemmer = Stemmer.Stemmer('english')
 f = open("./stopwords.pickle",'rb')
 stop_words = pickle.load(f)
+stop_words = set(stop_words)
 f.close()
+
+print(len(stop_words))
 
 f = open("./progress_file.txt",'r')
 number_docs = int(f.readline())
@@ -21,13 +24,35 @@ weights_for_terms = {}
 for i in term_keys:
     weights_for_terms[i] = 1
 
+weights_for_terms['t'] = 5
+weights_for_terms['i'] = 2
 print(number_docs)
 field_queries = {}
 
 query = sys.argv[1]
-query_words = []
 query_split = query.split(':')
 flag = 0
+
+def get_ranked_heap(relevant_docs):
+    ranked_docs = []
+    final_docs = []
+    
+    i = 0
+    for docID in relevant_docs:
+        heapq.heappush(ranked_docs, (relevant_docs[docID],docID))
+        i = i + 1
+        if i > 10:
+            x = heapq.heappop(ranked_docs)
+    
+    
+    for i in range(10):
+        final_docs.append(heapq.heappop(ranked_docs)[1])
+        if len(ranked_docs) == 0:
+            break
+    
+    final_docs = final_docs[::-1]
+    return final_docs
+
 
 if len(query_split)>1:
     flag = 1
@@ -60,13 +85,15 @@ if flag == 1:
                     index = json.load(f)
                     f.close()
                     
+                    if w not in index:
+                        continue
+
                     idf = math.log(number_docs/len(index[w][key]))
                     for docID in index[w][key]:
                         if docID not in relevant_docs:
                             relevant_docs[docID] = 0
                         relevant_docs[docID] = math.log(index[w][key][docID] + 1) * idf * weights_for_terms[key]
                     
-                    relevant_docs["idf"] = idf
                     continue
                 
                 first_char = w[0]
@@ -77,6 +104,9 @@ if flag == 1:
                 f = open("inverted_index/" + str(two_char) + ".json")
                 index = json.load(f)
                 f.close()
+
+                if w not in index:
+                    continue
                     
                 idf = math.log(number_docs/len(index[w][key]))
 
@@ -91,32 +121,75 @@ if flag == 1:
     print(json.dumps(field_queries,indent=4))
     print(json.dumps(relevant_docs,indent=4))
 
-    ranked_docs = []
-    
-    i = 0
-    for docID in relevant_docs:
-        heapq.heappush(ranked_docs, (-relevant_docs[docID],docID))
-        i = i + 1
-        if i > 10:
-            x = heapq.heappop(ranked_docs)
-    
-    print(ranked_docs)
-
-    final_docs = []
-    print()
-    print()
-    for i in range(10):
-        final_docs.append(heapq.heappop(ranked_docs)[1])
-        if len(ranked_docs) == 0:
-            break
-    
+    final_docs = get_ranked_heap(relevant_docs)
     for i in final_docs:
         file_num = (int(i) // 20000) + 1
         file_path = "inverted_index/t_d" + str(file_num) + ".txt"
         f = open(file_path,'r')
         t_dict = json.load(f)
         f.close()
-        print(t_dict[str(i)])
+        print(i,t_dict[str(i)])
 
 else:
-    print(query.split(' '))
+    query_words = query.split(' ')
+    relevant_docs = {}
+    for w in query_words:
+        if w == "":
+            continue
+        
+        w = w.lower()
+        if w in stop_words:
+            continue
+
+        w = snow_stemmer.stemWord(w)
+        try:
+            if len(w) == 1:
+                f = open("inverted_index/" + str(w) + ".json")
+                index = json.load(f)
+                f.close()
+                
+                if w not in index:
+                    continue
+
+                for key in list(index[w].keys()):
+                    idf = math.log(number_docs/len(index[w][key]))
+                    for docID in index[w][key]:
+                        if docID not in relevant_docs:
+                            relevant_docs[docID] = 0
+                        relevant_docs[docID] = math.log(index[w][key][docID] + 1) * idf * weights_for_terms[key]
+                
+                continue
+
+            first_char = w[0]
+            second_char = w[1]
+
+            two_char = first_char + second_char
+
+            f = open("inverted_index/" + str(two_char) + ".json")
+            index = json.load(f)
+            f.close()
+
+            if w not in index:
+                continue
+            
+            for key in list(index[w].keys()):
+
+                idf = math.log(number_docs/len(index[w][key]))
+                for docID in index[w][key]:
+                    if docID not in relevant_docs:
+                        relevant_docs[docID] = 0
+                    relevant_docs[docID] = relevant_docs[docID] + math.log(index[w][key][docID] + 1) * idf * weights_for_terms[key]
+            
+        except:
+            continue
+    
+    print(json.dumps(relevant_docs,indent=4))
+
+    final_docs = get_ranked_heap(relevant_docs)
+    for i in final_docs:
+        file_num = (int(i) // 20000) + 1
+        file_path = "inverted_index/t_d" + str(file_num) + ".txt"
+        f = open(file_path,'r')
+        t_dict = json.load(f)
+        f.close()
+        print(i,t_dict[str(i)])
