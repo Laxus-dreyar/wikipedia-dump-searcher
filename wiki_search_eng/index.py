048341154969a69ff061961b9e705531f1256265
Mrinal <mrinalkhubchandani13@gmail.com>
2021-09-16 23:32:35 +0530

    added splitting

--- wiki_search_eng/index.py
+++ wiki_search_eng/index.py
@@ -74,7 +74,7 @@ def add_to_index(t,key):
             temp[stemmed] = temp[stemmed] + 1
     
     for w in temp:
-
+        w = str(w)
         if (len(w) == 1) or (len(w) == 2):
             
             if w not in all_dict[w]:
@@ -105,7 +105,7 @@ def write_to_file(saving_path):
         if not all_dict[key]:
             continue
 
-        file_key_path = saving_path + key + ".json"
+        file_key_path = saving_path + key
         if os.path.isfile(file_key_path):
             f = open(file_key_path,'r')
             stored_index = json.load(f)
@@ -153,6 +153,8 @@ step = 0
 
 dump_path = sys.argv[1]
 saving_path = sys.argv[2]
+if saving_path[-1] != "/":
+    saving_path = saving_path + "/"
 
 make_dict()
 
@@ -249,22 +251,81 @@ for event,elem in iter(ET.iterparse(dump_path, events=("start", "end"))):
     if elem.tag == '{http://www.mediawiki.org/xml/export-0.10/}page' and event == "end":
         elem.clear()
 
-    
-
-# with open(saving_path,'w+') as f:
-#     f.write(json.dumps(index))
-#     f.close()
 
 write_to_file(saving_path)
 write_title_dict()
-
-# with open('./index.json','w') as f:
-#     f.write(json.dumps(all_dict, indent=0, separators=(",", ":")).replace("\n", ""))
-#     f.close()
 
 print("time taken = ", time.time()-start_time)
 
 print(docID)
 f = open("progress_file.txt",'w')
 f.write(str(docID))
+f.close()
+
+list_of_file_names = os.listdir(saving_path)
+
+list_of_file_names.sort()
+
+times_split = {}
+start_word = {}
+total_size = 0
+benchmark = int(100*1000)
+count = 0
+
+for file_name in list_of_file_names:
+
+    this_file_name = saving_path + file_name
+    if (len(file_name) > 2) and (file_name[0] == 't') and (file_name[1] == '_'):
+        continue
+
+    if (len(file_name) == 1) or ((len(file_name) == 2)): 
+        continue
+    size = int(os.path.getsize(this_file_name))
+    total_size = total_size + size
+
+    number_splits_req = 0
+    if size > benchmark:
+        count = count + 1
+        number_splits_req = 2 * size // benchmark + 1
+        
+        f = open(this_file_name,'r')
+        word_index = json.load(f)
+        f.close()
+
+        word_list = list(word_index.keys())
+        word_list.sort()
+
+        number_words_in_each_split = len(word_list) // number_splits_req
+        left_over = len(word_list) % number_splits_req
+
+        for i in range(number_splits_req):
+            new_dict = {}
+            for j in range(number_words_in_each_split):
+                cur_index = number_words_in_each_split*i + j
+                cur_word = word_list[cur_index]
+                new_dict[cur_word] = word_index[cur_word]
+            
+            new_saving_path = saving_path + file_name + "_" + str(i)
+            f = open(new_saving_path,'w')
+            f.write(json.dumps(new_dict, indent=0, separators=(",", ":")).replace("\n", ""))
+            f.close()
+
+            try:
+                new_dict_keys = list(new_dict.keys())
+                new_dict_keys.sort()
+                start_word[file_name + "_" + str(i)] = new_dict_keys[0]
+            except:
+                print(len(new_dict_keys),new_saving_path,file_name,size)
+        
+        os.remove(this_file_name)
+
+    times_split[file_name] = number_splits_req
+
+print(total_size,count)
+f = open("./" + "times_split.txt",'w')
+f.write(json.dumps(times_split, indent=0, separators=(",", ":")).replace("\n", ""))
+f.close()
+
+f = open("./" + "first_word.txt",'w')
+f.write(json.dumps(start_word, indent=0, separators=(",", ":")).replace("\n", ""))
 f.close()
